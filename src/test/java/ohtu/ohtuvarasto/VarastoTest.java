5b007939b8e5f5267c969ec2a21c8ce43bdd40b3
Mona Kulmala <mlkulmala@hotmail.com>
2021-11-15 10:52:19 +0200

    build.gradle ja uusia testejä

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -18,6 +18,7 @@ public class VarastoTest {
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        varastoAlkusaldolla = new Varasto(10, 5);
     }
 
     @Test
@@ -64,5 +65,28 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+
+    @Test
+    public void varastonSaldoaEiVoiYlittaa() {
+        varasto.lisaaVarastoon(11);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastonSaldoaEiVoiAlittaa() {
+        varasto.otaVarastosta(1);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void konstruktoriLuoVarastonOikeallaAlkusaldolla() {
+        assertEquals(5, varastoAlkusaldolla.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldollisenVarastonTilavuusOikein() {
+        assertEquals(10, varastoAlkusaldolla.getTilavuus(), vertailuTarkkuus);
+    }
+
 
 }
